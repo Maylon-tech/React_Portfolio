414614e0e14930adc8377ec60147913f7923a2ab
Maylon-tech <minatokiroisen77@gmail.com>
2023-06-11 23:21:24 +0900

    some configuration on projects

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -20,16 +20,47 @@ export const PaddingContainer = styled.div`
     padding-right: ${({ right }) => right};
 `
 
-export const Containe = styled.div`
+export const FlexContaine = styled.div`
     display: flex;
     justify-content: ${({ justify }) => justify};
     align-items: ${({ align }) => align};
     gap: ${({ gap }) => gap};
     flex-direction: ${({ direction }) => direction};
 
-    > div {
+    & > div {
         flex: ${({ fullWidthChild }) => fullWidthChild && 1};
     }
 `
 
-export const Heading = styled(PaddingContainer)``
+export const Heading = styled(PaddingContainer)`
+    color: ${({ theme }) => theme.colors.white};
+    text-align: ${({ align }) => align};
+    font-size: ${({ size }) => {
+        switch(size) {
+            case 'h1':
+                return '4.5rem';
+            case 'h2':
+                return '3rem';
+            case 'h3':
+                return '2rem';
+            case 'h4':
+                return '1.2rem';
+            default:
+                return;
+        }
+    }};
+`
+
+export const BlueText = styled.span`
+    color: ${({ theme }) => theme.colors.secondary};
+`
+
+export const ParaText = styled(PaddingContainer)`
+    
+`
+
+export const IconContainer = styled.div`
+    font-size: ${({ size }) => size};
+    cursor: pointer;
+    color: ${( color, theme ) => }size;
+`
